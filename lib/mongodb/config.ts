040ffb50b88ef95ab95761c9fd205ce4376651bb
Keming He <keminghe.career@gmail.com>
2025-02-26 00:06:45 -0500

    refactor(lib/**/config.ts): bettered env check
    added util function isNonemptry string for more rounded env check without exposing env

--- lib/mongodb/config.ts
+++ lib/mongodb/config.ts
@@ -2,10 +2,14 @@ import 'server-only';
 
 import type { MongoClientOptions } from 'mongodb';
 
+import { isNonemptyString } from '@/utils/is-nonempty-string';
+
 // Verify MongoDB URI is present in the environment
-if (!process.env.MONGODB_ATLAS_URI) {
-  throw new Error('MONGODB_ATLAS_URI is not defined in the environment');
+if (!isNonemptyString(process.env.MONGODB_ATLAS_URI)) {
+  throw new Error(
+    'MONGODB_ATLAS_URI is not defined or empty in the environment',
+  );
 }
 
-export const uri: string = process.env.MONGODB_ATLAS_URI;
+export const uri: string = process.env.MONGODB_ATLAS_URI as string;
 export const options: MongoClientOptions = {};
